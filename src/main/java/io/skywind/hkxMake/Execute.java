30f4e24332bd7db28cfe0ee32bd80acc1576cbf0
yooksi <yooksi@zoho.com>
2019-04-21 20:23:06 +0200

    Add process execution class

---
+++ src/main/java/io/skywind/hkxMake/Execute.java
@@ -0,0 +1,38 @@
+package io.skywind.hkxMake;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+
+public class Execute {
+
+	/**
+	 * Use {@code ProcessBuilder} to start a new application or script process.<br>
+	 * <i>Note that if you start a console application this way it will run hidden</i>
+	 * 
+	 * @param process path to the application or script we want to start
+	 * @param wait pause current thread and wait for process to terminate
+	 * @param log redirect process input stream to console
+	 * @return instance of the process started or {@code null} if an error occurred
+	 */
+	public static Process start(String process, String[] args, boolean wait, boolean log) {
+		
+		Logger.print(Logger.Level.DEBUG, "Starting new process %s" + 
+				((wait) ? " and waiting for it to terminate" : ""), process);
+		try {
+			String command = process + ((args != null) ? " " + String.join(" ", args) : "");
+			ProcessBuilder builder = new ProcessBuilder(command);
+			if (log == true) {
+				builder.inheritIO();
+			}
+			Process proc = builder.start();
+			if (wait == true) proc.waitFor();
+			return proc;
+		} 
+		catch (IOException | InterruptedException e) {
+			Logger.print(Logger.Level.ERROR, e, "Unable to start new process %s", process);
+			return null;
+		}
+	}
+}
